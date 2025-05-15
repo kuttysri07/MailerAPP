bad39dc2b5ad76659156822e7949b3afe3b4669b
Sriram <sriram29072002@gmail.com>
2025-05-15 11:08:56 +0530

    added compression

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
 import mailRouter from "./Router/sendRouter.js";
-import resumeRouter from "./Router/resumeRouter.js"
+import resumeRouter from "./Router/resumeRouter.js";
 import databaseConnection from "./Database/db.js";
+import compression from "compression";
 
 const app = express();
 const Port = 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use(compression());
 
 databaseConnection();
 
